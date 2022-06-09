dfab450baf2d997b887ed95e606becaff038a3e8
timmy <timothy.ndung'u@student.moringaschool.com>
2022-06-09 11:22:12 +0300

    deployment attempt

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -157,8 +157,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
