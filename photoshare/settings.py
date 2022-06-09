901ec2c5898db6a02050aad69ff60e6d61985d30
timmy <timothy.ndung'u@student.moringaschool.com>
2022-06-09 11:18:04 +0300

    deployment attempt

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -109,7 +109,7 @@ WSGI_APPLICATION = 'photoshare.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
